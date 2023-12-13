857ac17dea0f19c1b2eb6d6ce5d27518dcd63192
Ihor Vansach <ihor@magefan.com>
2023-12-13 16:27:25 +0200

    Refactored JS block

---
+++ Block/JsScript.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Copyright © Magefan (support@magefan.com). All rights reserved.
+ * Please visit Magefan.com for license details (https://magefan.com/end-user-license-agreement).
+ */
+
+declare(strict_types=1);
+
+namespace Magefan\Community\Block;
+
+use Magento\Framework\View\Element\Template;
+
+class JsScript extends Template
+{
+    /**
+     * @var array
+     */
+    static private $rendered = [];
+
+    /**
+     * @var string
+     */
+    private $jsMethod;
+
+    /**
+     * @param string $method
+     * @return JsScript
+     */
+    public function setMethod(string $method): JsScript
+    {
+        $this->jsMethod = $method;
+        return $this;
+    }
+
+    public function getTemplate()
+    {
+        if (!$this->_template) {
+            $this->_template = 'Magefan_Community::js/' . $this->jsMethod . '.phtml';
+        }
+        return parent::getTemplate();
+    }
+
+    /**
+     * @return string
+     */
+    public function toHtml() {
+        if (isset(self::$rendered[$this->jsMethod])) {
+            return '';
+        }
+        self::$rendered[$this->jsMethod] = 1;
+        return parent::toHtml();
+    }
+
+
+}
