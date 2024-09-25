def34da86f4553c06d48f398f12962df10922528
ivan.hrytsai <ivan.hrytsai@magefan.com>
2024-09-25 13:31:36 +0300

    unique dispatch name

--- Controller/Adminhtml/Actions.php
+++ Controller/Adminhtml/Actions.php
@@ -236,14 +236,14 @@ abstract class Actions extends \Magento\Backend\App\Action
             }
             $model->addData($params);
 
-            $this->_eventManager->dispatch('magefan_controller_actions_before_save', ['model' => $model]);
+            $this->_eventManager->dispatch('magefan_blog_' . $this->getRequest()->getControllerName()  . '_form__before_save', ['model' => $model]);
 
             $this->_beforeSave($model, $request);
             $model->save();
 
-            $this->_eventManager->dispatch('magefan_controller_actions_after_save');
-
             $this->_afterSave($model, $request);
+
+            $this->_eventManager->dispatch('magefan_blog_' . $this->getRequest()->getControllerName()  . '_form__after_save', ['model' => $model]);
 
             $this->messageManager->addSuccess(__('%1 has been saved.', $model->getOwnTitle()));
             $this->_setFormData(false);
@@ -526,7 +526,7 @@ abstract class Actions extends \Magento\Backend\App\Action
 
             if ($id && $load) {
                 $this->_model->load($id);
-                $this->_eventManager->dispatch('magefan_controller_actions_load_model_after',['model' => $this->_model]);
+                $this->_eventManager->dispatch('magefan_blog_' . $this->getRequest()->getControllerName()  . '_form_load_model_after', ['model' => $this->_model]);
             }
         }
         return $this->_model;
