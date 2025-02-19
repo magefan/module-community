a7085754fa61a14ab4bbca3501cf83f7a031cd34
Ihor Vansach <ihor@magefan.com>
2025-02-19 15:55:06 +0200

    Fix missing general seaction in module config

--- Block/Adminhtml/System/Config/Form/Info.php
+++ Block/Adminhtml/System/Config/Form/Info.php
@@ -169,7 +169,7 @@ class Info extends \Magento\Config\Block\System\Config\Form\Field
                 src: url("' . $this->getViewFileUrl("Magefan_Community::fonts/variable/LDIoaomQNQcsA88c7O9yZ4KMCoOg4Ko20yw.woff2") .'") format("woff2");
                 unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
             }
-            .section-config a[id$="_general-head"] {display: none;}
+            .section-config.active a[id$="_general-head"] {display: none;}
             
             .section-info {display: flex;flex-wrap: wrap;justify-content: space-between;gap: 36px;border: 1px solid rgba(0,0,0,0.1);background: #F8F8F8;padding: 24px 24px;font-size: 18px;font-weight: 500;font-family: "Plus Jakarta Sans", sans-serif;margin-bottom: 30px;}
             .section-info .col-info {display: flex;align-items: flex-start;}
