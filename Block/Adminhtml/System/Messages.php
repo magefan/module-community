22e9cbfdfd94d98f4660fd5ef56cf34f22e12b14
ivan.hrytsai <ivan.hrytsai@magefan.com>
2025-02-21 11:04:19 +0200

    12850-Support-updates-renewal-admin-notifications

---
+++ Block/Adminhtml/System/Messages.php
@@ -0,0 +1,284 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Magefan\Community\Block\Adminhtml\System;
+
+use Magefan\Community\Api\GetModuleInfoInterface;
+use Magefan\Community\Api\GetModuleVersionInterface;
+use Magefan\Community\Model\Config;
+use Magefan\Community\Model\SectionFactory;
+use Magento\Backend\Block\Template\Context;
+use Magento\Backend\Model\Auth\Session as AuthSession;
+use Magento\Framework\App\ResourceConnection;
+use Magento\Framework\Config\DomFactory;
+use Magento\Framework\Module\Dir\Reader as ModuleDirReader;
+use Magento\Framework\App\Route\Config as RouteConfig;
+
+class Messages extends \Magento\Backend\Block\Template
+{
+    protected $latestVersion = null;
+    protected $currentVersion = null;
+    protected $extensionName = null;
+
+    /**
+     * @var Config
+     */
+    private $config;
+
+    /**
+     * @var RouteConfig
+     */
+    private $routeConfig;
+
+    /**
+     * @var ModuleDirReader
+     */
+    private $moduleDirReader;
+
+    /**
+     * @var DomFactory
+     */
+    private $domFactory;
+
+    /**
+     * @var AuthSession
+     */
+    private $authSession;
+
+    /**
+     * @var ResourceConnection
+     */
+    private $resourceConnection;
+
+    /**
+     * @var SectionFactory
+     */
+    private $sectionFactory;
+
+    /**
+     * @var GetModuleInfoInterface|mixed
+     */
+    private $getModuleInfo;
+
+    /**
+     * @var GetModuleVersionInterface|mixed
+     */
+    private $getModuleVersion;
+
+    /**
+     * @var \Magefan\Community\Model\MessagePool
+     */
+    private $messagePool;
+
+    /**
+     * @param Context $context
+     * @param Config $config
+     * @param RouteConfig $routeConfig
+     * @param ModuleDirReader $moduleDirReader
+     * @param DomFactory $domFactory
+     * @param AuthSession $authSession
+     * @param ResourceConnection $resourceConnection
+     * @param SectionFactory $sectionFactory
+     * @param \Magefan\Community\Model\MessagePool $messagePool
+     * @param array $data
+     * @param GetModuleInfoInterface|null $getModuleInfo
+     * @param GetModuleVersionInterface|null $getModuleVersion
+     */
+    public function __construct(
+        Context $context,
+        Config $config,
+        RouteConfig $routeConfig,
+        ModuleDirReader $moduleDirReader,
+        DomFactory $domFactory,
+        AuthSession $authSession,
+        ResourceConnection $resourceConnection,
+        SectionFactory $sectionFactory,
+        \Magefan\Community\Model\MessagePool $messagePool,
+        array $data = [],
+        GetModuleInfoInterface $getModuleInfo = null,
+        GetModuleVersionInterface $getModuleVersion = null
+    ) {
+        parent::__construct($context, $data);
+        $this->cacheManager = $context->getCache();
+        $this->config = $config;
+        $this->routeConfig = $routeConfig;
+        $this->moduleDirReader = $moduleDirReader;
+        $this->domFactory = $domFactory;
+        $this->authSession = $authSession;
+        $this->resourceConnection = $resourceConnection;
+        $this->sectionFactory = $sectionFactory;
+        $this->messagePool = $messagePool;
+        $this->getModuleInfo = $getModuleInfo ?: \Magento\Framework\App\ObjectManager::getInstance()
+            ->get(GetModuleInfoInterface::class);
+        $this->getModuleVersion = $getModuleVersion ?: \Magento\Framework\App\ObjectManager::getInstance()->get(
+            \Magefan\Community\Api\GetModuleVersionInterface::class
+        );
+    }
+
+    /**
+     * @return array|string[]
+     */
+    public function getNotificationData()
+    {
+        $data = $this->messagePool->getAll($this->getRequest()->getFullActionName());
+        if ($data) {
+            return $data;
+        }
+        return [str_replace(['Extra', 'Plus'], '', $this->getFormattedModuleName()) => 'all'];
+    }
+    /**
+     * @return bool
+     */
+    public function isEnabled() {
+        foreach ($this->_storeManager->getStores() as $store) {
+            if ($this->config->getConfig($this->getConfigSection() . '/general/enabled', $store->getId())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * @return array|false|\Magento\Framework\DataObject|mixed
+     */
+    public function getModuleInfo() {
+        $extensionName = str_replace(['Extra', 'Plus'], '', $this->getFormattedModuleName());
+        return $extensionName ? $this->getModuleInfo->execute($extensionName) : false;
+    }
+
+    /**
+     * @return string
+     */
+    private function getCurrentVersion() {
+        if ($this->currentVersion === null) {
+            $moduleName = $this->getFormattedModuleName();
+            $this->currentVersion = $this->getModuleVersion->execute($moduleName);
+        }
+        return $this->currentVersion;
+    }
+
+    /**
+     * @return string
+     */
+    private function getLatestVersion() {
+        if ($this->latestVersion === null) {
+            try {
+                $this->latestVersion = $this->getModuleInfo()->getVersion();
+            } catch (\Exception $e) {
+                $this->latestVersion = '';
+            }
+        }
+        return $this->latestVersion;
+    }
+
+    /**
+     * @return bool|int
+     */
+    public function needToUpdate() {
+        return version_compare($this->getCurrentVersion(), $this->getLatestVersion(), '<');
+    }
+
+    /**
+     * @param $name
+     * @return void
+     */
+    public function setExtensionName($name = null) {
+        if ($name) {
+            $this->extensionName = $name;
+            return;
+        }
+        $frontModule = $this->routeConfig->getModulesByFrontName($this->getRequest()->getModuleName());
+
+        if (!empty($frontModule[0]) && strpos($frontModule[0], 'Magefan_') !== false) {
+            $this->extensionName = $frontModule[0];
+        } else {
+            $section = $this->sectionFactory->create( ['name' => $this->getRequest()->getParam('section')]);
+            $this->extensionName = $section->getModuleName();
+        }
+    }
+
+    /**
+     * @return mixed|null
+     */
+    private function getExtensionName() {
+        if ($this->extensionName === null) {
+            $this->setExtensionName();
+        }
+        return $this->extensionName;
+    }
+
+    /**
+     * @return mixed|string|null
+     */
+    private function getFormattedModuleName() {
+        $moduleName = $this->getExtensionName();
+        return str_starts_with($moduleName, 'Magefan_') ? $moduleName : 'Magefan_' . $moduleName;
+    }
+
+    /**
+     * @return null
+     */
+    public function getConfigSection() {
+
+        if ($this->getRequest()->getParam('section')) {
+            return $this->getRequest()->getParam('section');
+        }
+
+        $moduleName = $this->getFormattedModuleName();
+        if (!$moduleName) {
+            return null;
+        }
+
+        $configPath = $this->moduleDirReader->getModuleDir(
+                \Magento\Framework\Module\Dir::MODULE_ETC_DIR,
+                $moduleName
+            ) . '/adminhtml/system.xml';
+
+        if (!file_exists($configPath)) {
+            return null;
+        }
+
+        $dom = $this->domFactory->createDom(['xml' => file_get_contents($configPath)]);
+        $xpath = new \DOMXPath($dom->getDom());
+
+        foreach ($xpath->query('/config/system/section') as $sectionNode) {
+            return $sectionNode->getAttribute('id');
+        }
+
+        return null;
+    }
+
+    /**
+     * @return bool
+     */
+    public function canUpgradePlan() {
+        $maxPlan = $this->getModuleInfo()->getMaxPlan();
+        $extensionName = str_replace(['Extra', 'Plus'],'' , $this->getFormattedModuleName());
+        return $maxPlan && !$this->getModuleVersion->execute($extensionName. ucfirst($maxPlan));
+    }
+
+    /**
+     * @param string $event
+     * @return bool
+     */
+    public function allowShowMessage(string $event) {
+        $adminUser = $this->authSession->getUser();
+        if (!$adminUser) {
+            return false;
+        }
+
+        $connection = $this->resourceConnection->getConnection();
+        $tableName = $connection->getTableName('mf_message_remind_later');
+
+        $select = $connection->select()
+            ->from($tableName, 'user_id')
+            ->where('user_id = ?', $adminUser->getId())
+            ->where('event = ?', $event)
+            ->where('module_name = ?', str_replace(['Magento 2 ', ' Extension'], '', $this->getModuleInfo()->getProductName()))
+            ->where('created_at >= ?', (new \DateTime('-1 day'))->format('Y-m-d H:i:s'))
+            ->limit(1);
+
+        return !$connection->fetchOne($select);
+    }
+}
---
+++ Controller/Adminhtml/RemindLater/Index.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Copyright © Magefan (support@magefan.com). All rights reserved.
+ * Please visit Magefan.com for license details (https://magefan.com/end-user-license-agreement).
+ */
+
+declare(strict_types=1);
+
+namespace Magefan\Community\Controller\Adminhtml\RemindLater;
+
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\Controller\ResultFactory;
+use Magento\Framework\Exception\NoSuchEntityException;
+
+class Index extends \Magento\Backend\App\Action
+{
+    /**
+     * @var \Magento\Backend\Model\Auth\Session
+     */
+    private $authSession;
+
+    /**
+     * @var \Magento\Framework\App\ResourceConnection
+     */
+    private $resource;
+
+    /**
+     * @param Context $context
+     * @param \Magento\Backend\Model\Auth\Session $authSession
+     * @param \Magento\Framework\App\ResourceConnection $resource
+     */
+    public function __construct(
+        Context $context,
+        \Magento\Backend\Model\Auth\Session $authSession,
+        \Magento\Framework\App\ResourceConnection $resource
+    )
+    {
+        parent::__construct($context);
+        $this->authSession = $authSession;
+        $this->resource = $resource;
+    }
+
+    /**
+     * @return \Magento\Framework\App\ResponseInterface|\Magento\Framework\Controller\Result\Json|(\Magento\Framework\Controller\Result\Json&\Magento\Framework\Controller\ResultInterface)|\Magento\Framework\Controller\ResultInterface
+     */
+    public function execute()
+    {
+        try {
+            if (!$adminUser = $this->authSession->getUser()) {
+                throw new NoSuchEntityException(__('Admin User is not found.'));
+            }
+            if (!$event = $this->_request->getParam('event')) {
+                throw new NoSuchEntityException(__('Magento Event is not provided.'));
+            }
+
+            if (!$moduleName = $this->_request->getParam('module')) {
+                throw new NoSuchEntityException(__('Magento Event is not provided.'));
+            }
+
+            $connection = $this->resource->getConnection();
+            $tableName = $connection->getTableName('mf_message_remind_later');
+
+            $data = [
+                'user_id' => $adminUser->getId(),
+                'module_name' => $moduleName,
+                'event' => $event
+            ];
+
+            $connection->insert($tableName, $data);
+            $result = ['success' => true];
+        } catch (\Exception $e) {
+            $result = ['error' => $e->getMessage(), 'errorcode' => $e->getCode()];
+        }
+        return $this->resultFactory->create(ResultFactory::TYPE_JSON)->setData($result);
+    }
+}
